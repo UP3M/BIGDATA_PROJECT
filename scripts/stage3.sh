9ba9313f388a3ddf8bbaf93b77c0e77e8f80aaba
Made Oka Resia Wedamerta <69470640+UP3M@users.noreply.github.com>
2023-05-12 00:11:47 +0300

    .

--- scripts/stage3.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-rm -r models/best_model_rf.model
-rm -r models/best_model_lr.model
-spark-submit --jars /usr/hdp/current/hive-client/lib/hive-metastore-1.2.1000.2.6.5.0-292.jar,/usr/hdp/current/hive-client/lib/hive-exec-1.2.1000.2.6.5.0-292.jar scripts/model.py
--- scripts/stage4.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#pip install streamlit --ignore-installed
-streamlit run scripts/dashboard.py --server.port 60000
